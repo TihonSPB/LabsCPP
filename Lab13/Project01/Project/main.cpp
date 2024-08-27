1b350610230bf3dc22e6655a945daa22d58bb499
TihonSPB <tihon.spb@gmail.com>
2024-08-28 00:04:16 +0300

    Lab 13

---
+++ Lab13/Project01/Project/main.cpp
@@ -0,0 +1,97 @@
+﻿//Применение полиморфизма
+//Реализация полиморфного вызова
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+// Класс единицы хранения
+class Item
+{
+private:
+	string title; // название
+	double price; // цена
+public:
+	virtual void getdata() // виртуальный метод ввода
+	{
+		cout << "\nВведите заголовок: ";
+		cin >> title;
+		cout << "Введите цену: ";
+		cin >> price;
+	}
+	virtual void putdata() // виртуальный метод вывода
+	{
+		cout << "\nЗаголовок: " << title;
+		cout << "\nЦена: " << price;
+	}
+};
+
+// Класс производный от базового класса Item
+class Paperbook : public Item
+{
+private:
+	int pages; // переменная учета количества страниц
+public:
+	void getdata() // переопределение метода ввода базового класса Item
+	{
+		Item::getdata(); 
+		cout << "Введите число страниц: ";
+		cin >> pages;
+	}
+	void putdata() // переопределение метода вывода базового класса Item
+	{
+		Item::putdata(); 
+		cout << "\nСтраниц: " << pages;
+	}
+};
+
+// Класс производный от базового класса Item
+class AudioBook : public Item
+{
+private:
+	double time; // переменная учета времени звучания
+public:
+	void getdata() // переопределение метода ввода базового класса Item
+	{
+		Item::getdata(); 
+		cout << "Введите время звучания : ";
+		cin >> time;
+	}
+	void putdata() // переопределение метода вывода базового класса Item
+	{
+		Item::putdata();
+		cout << "\nВремя звучания : " << time;
+	}
+};
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+
+	Item* pubarr[100]; // массив указателей на класс Item
+	int n = 0;
+	char choice;
+
+	// запрос у пользователя данные о выборе варианта заполнения данных
+	do
+	{
+		cout << "\nВводить данные для книги или звукового файла(b / a) ? ";
+		cin >> choice;
+		if (choice == 'b')
+			pubarr[n] = new Paperbook; // новый объект класса Paperbook
+		else
+			pubarr[n] = new AudioBook; // новый объект класса AudioBook
+		pubarr[n++]->getdata();
+		cout << "Продолжать((у / n) ? ";
+		cin >> choice;
+	} while (choice == 'y');
+
+	// вывод данных о заполненных единицах хранения
+	for (int j = 0; j < n; j++) //цикл по всем объектам
+		pubarr[j]->putdata(); //вывести данные о публикации
+	cout << endl;
+
+	return 0;
+}
---
+++ Lab13/TestWork/Project/main.cpp
@@ -0,0 +1,50 @@
+﻿// Полиморфизм в системе классов учебного центра
+// main.cpp
+
+#include <iostream>
+#include <vector>
+#include "human.h"
+#include "student.h"
+#include "teacher.h"
+
+int main()
+{
+	system("chcp 1251");
+
+	// Оценки студента
+	std::vector<int> scores;
+	// Добавление оценок студента в вектор
+	scores.push_back(5);
+	scores.push_back(3);
+	scores.push_back(4);
+	scores.push_back(4);
+	scores.push_back(5);
+	scores.push_back(3);
+	scores.push_back(3);
+	scores.push_back(3);
+	scores.push_back(3);
+
+	// Создание студента – объект класса student 
+	student* stud = new student("Петров", "Иван", "Алексеевич", scores);
+
+	// Вывод полного имени студента используя унаследованный метод класса human
+	std::cout << stud->get_full_name() << std::endl;
+
+	// Вывод среднего балла студента
+	std::cout << "Средний балл : " << stud->get_average_score() << std::endl;
+
+
+	// Количество учебных часов за семестр у преподавателя
+	unsigned int teacher_work_time = 40;
+
+	// Создание преподавателя – объект класса teacher
+	teacher* tch = new teacher("Сергеев", "Дмитрий", "Сергеевич", teacher_work_time);
+
+	// Вывод полного имени преподавателя используя унаследованный метод класса human
+	std::cout << tch->get_full_name() << std::endl;
+
+	// Вывод количества учебных часов у преподавателя
+	std::cout << "Количество часов: " << tch->get_work_time() << std::endl;
+
+	return 0;
+}
